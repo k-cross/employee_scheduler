329db683b34ed92c3a81fb4aedce7f0f809303c1
k-cross <kenneth.cross@sjsu.edu>
2015-06-21 01:19:40 -0700

    Architectural Change Planning

--- src/semi_dp_scheduler.R
+++ src/semi_dp_scheduler.R
@@ -1,28 +1,31 @@
-#Written by: K-Cross
+# Written by: K-Cross
 
 # Major Changes
 # * No more randomization, DP used and all outcomes are stored for a single day
 # * Optimization factors are added
 
-#Initialzed variables
+# Initialzed variables
 availability = read.csv("../data/data_rep.csv")
 shiftClass = read.csv("../data/shift_class.csv")
 
-#Number of people
+# Number of people
 people = availability[,1]
 
-#initialize the position vectors for comparing choices
+# Initialize the position vectors for comparing choices
 position = availability[, 9:(dim(availability)[2] - 2)]
 
-#availability based on what day of week
+# Availability based on what day of week
 dayAvailability = availability[,2:8]
 weekDaySum = rowSums(dayAvailability)
 
-#availability based on how many days can work
+# Availability based on how many days can work
 workTime = availability[,dim(availability)[2] - 1]
 
 dayIndex = 0
 daysOpen = 7
+
+# Conditions
+wdCondition = 0 # binary condition, 0 = false
 
 optimizedSchedule = matrix("0", daysOpen, length(positions[1,]))
 colnames(optimizedSchedule) = colnames(positions)
